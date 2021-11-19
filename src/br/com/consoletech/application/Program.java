283446113d20127a16c055e37e38ba839a66ea34
flinraider <leonardo.teixeira.dev@gmail.com>
2021-11-19 11:51:57 -0300

    Classe Page

--- src/br/com/consoletech/application/Program.java
+++ src/br/com/consoletech/application/Program.java
@@ -1,25 +1,16 @@
 package br.com.consoletech.application;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class Program extends Application {
 	
+	Page pg = new Page();
+	
 	@Override
 	public void start(Stage stage) throws Exception {
+		Page.primaryPage(stage);
 		
-		FlowPane view = new FlowPane();
-		WebView wv = new WebView();
-		WebEngine engine = wv.getEngine();
-		engine.load("https://consoletech.com.br");
-		stage.setTitle("WEB");
-		view.getChildren().add(wv);
-		stage.setScene(new Scene(view));
-		stage.show();
 	}
 	
 	public static void main(String[] args) {
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module WebView {
 	
 	requires javafx.base;
+	requires javafx.fxml;
 	requires javafx.controls;
 	requires javafx.graphics;
 	requires javafx.web;
